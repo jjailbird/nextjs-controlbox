0c554f8c3b8c3f310258a29cccde6c8767198617
jjailbird <jjailbird@gmail.com>
2022-10-23 05:31:32 +0900

    Modify button size

--- utils/theme.js
+++ utils/theme.js
@@ -16,8 +16,8 @@ export const theme = createTheme({
             borderRadius: 10,
             border: 0,
             color: "white",
-            height: 1000,
-            width: 450,
+            height: 800,
+            width: 350,
             padding: "0 30px",
             boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
           }
@@ -29,8 +29,8 @@ export const theme = createTheme({
             borderRadius: 10,
             border: 0,
             color: "white",
-            height: 1000,
-            width: 450,
+            height: 800,
+            width: 350,
             padding: "0 30px",
             boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
           }
