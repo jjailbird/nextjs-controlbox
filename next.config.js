94b8d7c7db8920ece82a6522f3c19d6c7f1ea3bb
jjailbird <jjailbird@gmail.com>
2022-10-23 15:14:10 +0900

    Add commet for next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // for static page, if you run ssr remove below config
   images: {
     unoptimized: true
   }
+  // ---------------------------------------------------
 }
 
 module.exports = nextConfig
